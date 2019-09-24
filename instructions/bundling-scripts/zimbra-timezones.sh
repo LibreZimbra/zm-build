c75e809d3a82ae120856769f972b0ddebc8f183e
Enrico Weigelt, metux IT consult <info@metux.net>
2019-09-25 00:45:57 +0200

    fixup zimbra-timezone.sh

--- instructions/bundling-scripts/zimbra-timezones.sh
+++ instructions/bundling-scripts/zimbra-timezones.sh
@@ -6,12 +6,7 @@
 
 set -e
 
-#currentScript=`basename $0 | cut -d "." -f 1`                 # zimbra-store
-#currentPackage=`echo ${currentScript}build | cut -d "-" -f 2` # storebuild
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SRC_ROOT="$SCRIPT_DIR/../../../"
-
-( set -e ; cd "$SRC_ROOT/zm-timezones" && ./pkg-builder.pl )
 
 cp $SRC_ROOT/zm-timezones/build/dist/*.deb ${repoDir}/zm-build/${arch}
