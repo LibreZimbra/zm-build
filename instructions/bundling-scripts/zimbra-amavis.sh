68cc8a334fae4b2289f13343c402b78d9aa9680f
Enrico Weigelt, metux IT consult <info@metux.net>
2019-09-25 00:38:25 +0200

    1

--- instructions/bundling-scripts/zimbra-amavis.sh
+++ instructions/bundling-scripts/zimbra-amavis.sh
@@ -9,6 +9,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SRC_ROOT="$SCRIPT_DIR/../../../"
 
-( set -e ; cd "$SRC_ROOT/zm-amavis" && ./pkg-builder.pl )
+#( set -e ; cd "$SRC_ROOT/zm-amavis" && ./pkg-builder.pl )
 
-cp $SRC_ROOT/zm-amavis/build/dist/*.deb ${repoDir}/zm-build/${arch}
+#cp $SRC_ROOT/zm-amavis/build/dist/*.deb ${repoDir}/zm-build/${arch}
