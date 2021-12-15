c565e3831059ff583745202e5a345a86d428e99a
Enrico Weigelt, metux IT consult <info@metux.net>
2021-12-15 20:14:43 +0100

    build.pl: drop zm-mailbox from legacy build
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- instructions/FOSS_staging_list.pl
+++ instructions/FOSS_staging_list.pl
@@ -1,20 +1,5 @@
 @ENTRIES = (
    {
-      "dir"             => "zm-mailbox",
-      "ant_targets"     => ["pkg-after-plough-through-tests"],
-      "deploy_pkg_into" => "bundle",
-      "stage_cmd"       => sub {
-         SysExec("mkdir -p                                 $CFG{BUILD_DIR}/zm-mailbox/store-conf/");
-         SysExec("rsync -az store-conf/conf                $CFG{BUILD_DIR}/zm-mailbox/store-conf/");
-         SysExec("install -T -D store/build/dist/versions-init.sql $CFG{BUILD_DIR}/zm-mailbox/store/build/dist/versions-init.sql");
-      },
-   },
-   {
-      "dir"         => "zm-mailbox/store",
-      "ant_targets" => ["publish-store-test"],
-      "stage_cmd"   => undef,
-   },
-   {
       # This repo can be removed and made independent of zm-zextras
       # This cannot be done unless the packages from zm-timezones are pushed to public repo
       # This is already excluded in CircleCI builds
